8a49e5d2ba4132b6a4b02376dc8d82a3b3bf7c8a
kkobikim <kkobi.kim@gbike.io>
2024-01-15 09:22:57 +0900

    1

--- actions/getHourlyData.ts
+++ actions/getHourlyData.ts
@@ -6,7 +6,7 @@ export const getHourlyData = async ({
   lon: string
 }) => {
   const data = await fetch(
-    `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    `https://${process.env.VERCEL_URL}/api/weather/hourly?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
   )
   if (!data.ok) {
     throw new Error(`Failed to fetch data: Status Code ${data.status}`)
